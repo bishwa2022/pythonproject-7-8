784d1eb987074ed1f1817ba9388f83dfa80a44f9
bishwa2022 <bishwag@metropolia.fi>
2022-09-05 17:33:23 +0300

    new

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -1,52 +1,36 @@
+zander = int(input("What is the size of zander in cm "))
+ if zander < 42:
+     print("release fish because it is small", abs(zander - 42))
+ else:
+    print("enjoy fishing")
 
-list1 = []
-matchpoint = [9,4,3,8,10,6]
-print(matchpoint[0])
-print(matchpoint[3])
-print(matchpoint[5])
-print("total number:", matchpoint[0] + matchpoint[3] + matchpoint[5])
+Cruiseship = input("What is the cabin class")
+Lux = Cruiseship
+if "Lux" == Lux:
+  print("LUX: upper-deck cabin with a balcony")
+elif "A" == Lux:
+    print("A: above the car deck, equipped with a window")
+elif "B" == Lux:
+    print(print("B: windowless cabin above the car deck"))
+elif "C" == Lux:
+    print("C: windowless cabin below the car deck")
 
+gender = input("Are you male of female? ")
+hemoValue = float(input("Input your hemoglobin value (g/l): "))
+if gender == "male":
+    if hemoValue < 134:
+        print("Your hemoglobin value is low")
+    elif hemoValue > 167:
+        print("Your hemoglobin value is high")
+    else:
+        print("Your hemoglobin value is normal")
+else:
+    if hemoValue < 117:
+        print("Your hemoglobin value is low")
+    elif hemoValue > 155:
+        print("Your hemoglobin value is high")
+    else:
+        print("Your hemoglobin value is normal")
 
-numberlist = []
-numberlist.append(9)
-numberlist.append(3)
-numberlist.append(6)
-
-shoppinglist = ['apple', 'banana', 'fish', 'meat', 'chicken']
-print(shoppinglist)
-shoppinglist.insert(3, "4 eggs")
-print(shoppinglist)
-shoppinglist.pop(2)
-print(shoppinglist)
-shoppinglist.remove('banana')
-print(shoppinglist)
-
-name = []
-name = input("Enter your name:")
-index = 0
-while name != "" and index < len(name):
-    print(name[index])
-    index += 1
-
-name = input("Enter your name:")
-for character in name:
-    print(character)
-
-for number in range(1,10,2):
-    print(number)
-
-for number in range(4):
-    print("I am so cool!"):
-
-number = list(range(1,5))
-print(number)
-
-num = int(input("input a number, 0 or negative to exit:"))
-while num > 0:
-    result = 1
-    for i in range(1,num+1):
-         result = result * i
-    print(result)
-    num = int(input("input a number, 0 or negative to exit:"))
 
 
