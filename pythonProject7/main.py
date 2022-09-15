f27c7eb6e0dd560f98545d14fac33c9b6521dcd4
bishwa2022 <bishwag@metropolia.fi>
2022-09-15 15:38:18 +0300

    exercise

---
+++ pythonProject7/main.py
@@ -0,0 +1,28 @@
+"""names = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
+
+print(names[3])
+print(names[1])
+print(names[-2])
+print(names[1:3])
+print(names[2:])
+print(names)"""
+
+"""names = []
+
+name = input("Enter the first name or quit by pressing Enter: ")
+while name!="":
+    names.append(name)
+    name = input("Enter the next name or quit by pressing Enter: ")
+
+print(nimet)"""
+"""names = []
+
+name = input("Enter the first name or quit by pressing Enter: ")
+while name!="":
+    names.append(name)
+    name = input("Enter the next name or quit by pressing Enter: ")
+
+for n in names:
+    print (f"Hello, {n}!")"""
+for n in names:
+    print(f"Hello, {n}!")
