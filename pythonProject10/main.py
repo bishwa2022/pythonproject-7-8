b9bfc88fefa36332094d5eaf8bbc702d1b3a76ad
bishwa2022 <bishwag@metropolia.fi>
2022-09-15 15:32:30 +0300

    exercise

---
+++ pythonProject10/main.py
@@ -0,0 +1,69 @@
+"""n = int(input("How many times to roll a dice"))
+dice_sum = 0
+for i in range(n):
+    dice = random.randit(1, 6)
+    dice_sum = dice_sum + dice
+print(f"\n The sum of the dices is: {dice_sum}")"""
+import random
+
+#phase 2
+"""numbers = []
+prompt = "Give me a number"
+s = input(prompt)
+while s != "":
+    numbers.append(float(s))
+    s = input(prompt)
+numbers.sort(revrse=True)
+print(numbers[0:5])"""
+#phase 3
+"""n = int(input("Give the number"))
+for i in range(2, int(math.sqrt(n))*1):
+    if n%i == 0:
+        print(f"Dividable by {i}")
+        break
+else:
+    print("It is a prime number")"""
+#phase 4
+"""cities = []
+for n in range(5):
+    cities.append(input("Give me the name of the city"))
+for city in cities:
+    print(city)"""
+#phase 5
+"""def dice_roll():
+    return random.randint(1, 6)
+
+while True:
+    dice = dice_roll()
+    print(f"Dice {dice}")
+    if dice == 6:
+        break"""
+#phase 6
+
+"""def make_even(l):
+    result = []
+    for i in range(len(l)):
+        if l[i] % 2 == 0:
+            result.append(l[i])
+    return result
+
+
+def print_list(prompt, l):
+    print(prompt, end=":")
+    for i in range(len(l)):
+        print(l[i], end=" ")"""
+#phase 7
+"""def pizzs_efficiency(d, price):
+    return price / math.pi * (d / 200.) ** 2
+
+
+p1_diameter = float(input("Give the diameter of the first pizza (in cm)?"))
+p1_price = float(input("Give the price of the first pizza (in euros)?"))
+p2_diameter = float(input("Give the diameter of the second pizza (in cm)?"))
+p2_price = float"""
+
+"""fruits = "orange", "banana", "Apple"
+print(fruits)"""
+
+games = {"Monopoly", }
+
