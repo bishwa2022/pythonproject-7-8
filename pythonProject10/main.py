853a163ba7c43167098fc309d1b3bcd5dadc6a87
bishwa2022 <bishwag@metropolia.fi>
2022-09-17 20:38:08 +0300

    excercise

--- pythonProject10/main.py
+++ pythonProject10/main.py
@@ -1,69 +1,64 @@
-"""n = int(input("How many times to roll a dice"))
-dice_sum = 0
-for i in range(n):
-    dice = random.randit(1, 6)
-    dice_sum = dice_sum + dice
-print(f"\n The sum of the dices is: {dice_sum}")"""
-import random
+"""print("Hello everyone")
+print("Welcome to python program")
+print("There is three line")"""
 
-#phase 2
-"""numbers = []
-prompt = "Give me a number"
-s = input(prompt)
-while s != "":
-    numbers.append(float(s))
-    s = input(prompt)
-numbers.sort(revrse=True)
-print(numbers[0:5])"""
-#phase 3
-"""n = int(input("Give the number"))
-for i in range(2, int(math.sqrt(n))*1):
-    if n%i == 0:
-        print(f"Dividable by {i}")
-        break
-else:
-    print("It is a prime number")"""
-#phase 4
-"""cities = []
-for n in range(5):
-    cities.append(input("Give me the name of the city"))
-for city in cities:
-    print(city)"""
-#phase 5
-"""def dice_roll():
-    return random.randint(1, 6)
+"""print(2 + 3)
+print(6 + 7 * 2)"""
 
-while True:
-    dice = dice_roll()
-    print(f"Dice {dice}")
-    if dice == 6:
-        break"""
-#phase 6
+"""print(2 + 3)
+print("2 + 3")"""
 
-"""def make_even(l):
-    result = []
-    for i in range(len(l)):
-        if l[i] % 2 == 0:
-            result.append(l[i])
-    return result
+"""sur_name = input("What is your surname")
+print("Hello" + sur_name)"""
+"""sur_name = input("What is your surname")
+print("Hello" + sur_name)"""
+
+"""first_name = "Bishwa"
+sur_name = "Ghimire"
+full_name = first_name + "" + sur_name
+print(full_name)"""
+"""name = input("What is your name")
+print(name)
+name = name + "Hello"
+print(name)"""
+"""number1 = 10
+number2 = "10"
+print(number1)
+print(number2)"""
+"""number = 10
+print("This is the number" + str(number))"""
+"""number = 10
+print(f"The number is")"""
+
+"""number = 1/5
+print(f"The number is {number:.2f}")"""
+
+"""sur_name = input("What is your sur_name?")
+print("Hello" + sur_name)"""
+"""first_name = "Bishwa"
+last_name = "Ghimire"
+full_name = first_name + "" + last_name
+print(full_name)"""
+"""name = input("What is your name?")
+print(name)
+name = name + "Hello"
+print(name)"""
+"""number1 = 10
+number2 = "10"
+print(number1)
+print(number2)"""
+
+"""number = 10
+print(number -2, number *6)"""
+"""number = 10
+print(f"The is number")"""
+"""number = 1/5
+print(f"The number is {number:.2f}")"""
+#phase
+def greet(first_name):
+    print("Hi there")
+    print("Welcome aboard")
 
 
-def print_list(prompt, l):
-    print(prompt, end=":")
-    for i in range(len(l)):
-        print(l[i], end=" ")"""
-#phase 7
-"""def pizzs_efficiency(d, price):
-    return price / math.pi * (d / 200.) ** 2
-
-
-p1_diameter = float(input("Give the diameter of the first pizza (in cm)?"))
-p1_price = float(input("Give the price of the first pizza (in euros)?"))
-p2_diameter = float(input("Give the diameter of the second pizza (in cm)?"))
-p2_price = float"""
-
-"""fruits = "orange", "banana", "Apple"
-print(fruits)"""
-
-games = {"Monopoly", }
+greet("Bishwa")
 
